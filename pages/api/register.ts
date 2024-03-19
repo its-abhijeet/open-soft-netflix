7f1c935d4431b5f96f044b581a37311c2fc78d6a
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2024-03-20 01:18:21 +0530

    abhijeet's branch

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,10 +1,13 @@
-import bcrypt from 'bcrypt';
-import { NextApiRequest, NextApiResponse } from 'next';
-import prismadb from '@/libs/prismadb';
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/libs/prismadb";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    if (req.method !== 'POST') {
+    if (req.method !== "POST") {
       return res.status(405).end();
     }
 
@@ -12,25 +15,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const existingUser = await prismadb.user.findUnique({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
     if (existingUser) {
-      return res.status(422).json({ error: 'Email taken' });
+      return res.status(422).json({ error: "Email taken" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prismadb.user.create({
       data: {
         email,
         name,
         hashedPassword,
-        image: '',
+        image: "",
         emailVerified: new Date(),
-      }
-    })
+      },
+    });
 
     return res.status(200).json(user);
   } catch (error) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { NextPageContext } from 'next';
-import { getSession } from 'next-auth/react';
+import React from "react";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
-import Navbar from '@/components/Navbar';
-import Billboard from '@/components/Billboard';
-import MovieList from '@/components/MovieList';
-import InfoModal from '@/components/InfoModal';
-import useMovieList from '@/hooks/useMovieList';
-import useFavorites from '@/hooks/useFavorites';
-import useInfoModalStore from '@/hooks/useInfoModalStore';
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import InfoModal from "@/components/InfoModal";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -16,21 +16,21 @@ export async function getServerSideProps(context: NextPageContext) {
   if (!session) {
     return {
       redirect: {
-        destination: '/auth',
+        destination: "/auth",
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 const Home = () => {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
-  const {isOpen, closeModal} = useInfoModalStore();
+  const { isOpen, closeModal } = useInfoModalStore();
 
   return (
     <>
@@ -42,7 +42,7 @@ const Home = () => {
         <MovieList title="My List" data={favorites} />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
